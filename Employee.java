66daac0e78a9aed044dc59722cb99eb0adfd2d76
subashsubash2502 <subashsubash2502.subash3979@gmail.com>
2022-09-02 14:19:44 +0530

    task

---
+++ Employee.java
@@ -0,0 +1,22 @@
+package org.cts;
+
+//class 1
+//Parent
+
+public class Employee {
+	public static int age = 50;
+	public static void add() {
+		int a =10;
+		a++;
+		System.out.println(a);
+					
+	}
+	
+	public static void main(String []args) {
+		
+	System.out.println(age);
+	
+		
+	}	
+
+}
---
+++ SampleExtendsEmployee.java
@@ -0,0 +1,25 @@
+package org.cts;
+
+//class 2
+//child
+
+public class SampleExtendsEmployee {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		//without extends
+		
+		System.out.println(Employee.age);
+		
+		int age = 15;
+		//with extends
+		System.out.println(age);
+		
+		
+		
+	}
+
+}
